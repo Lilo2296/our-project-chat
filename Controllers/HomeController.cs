144a6eb7c03aee2321c4d8066d0be94f99357781
lilo2296 <lilo2296@gmail.com>
2016-10-17 01:11:13 +0700

    Add full project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using SignalRChat.Models;
+
+namespace SignalRChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(User user)
+        {
+            return RedirectToAction("Index", "Chat", new { userId = user.Id });
+        }
+        public ActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
